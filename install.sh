e333d85c174935cdcc706eb1aad837ef9e791439
Aaron Meurer <asmeurer@gmail.com>
2022-08-17 17:31:47 -0600

    Do a recursive clone

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ set -e
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
-git clone https://github.com/data-apis/array-api-tests
+git clone --recursive-submodules https://github.com/data-apis/array-api-tests
 cd array-api-tests
 git checkout "$ARRAY_API_TESTS_VERSION"

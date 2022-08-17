beaafdcfea8d733bf9f04b740e653d2f99046305
Aaron Meurer <asmeurer@gmail.com>
2022-08-17 17:34:08 -0600

    Use actions/checkout to checkout array-api-tests

--- install.sh
+++ install.sh
@@ -4,7 +4,3 @@ set -e
 
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
-
-git clone --recursive-submodules https://github.com/data-apis/array-api-tests
-cd array-api-tests
-git checkout "$ARRAY_API_TESTS_VERSION"

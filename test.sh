41039d0bc3a9d6e366b4dcc164bba3a037f820f5
Aaron Meurer <asmeurer@gmail.com>
2022-08-25 11:17:39 -0600

    Better way of making the build not fail on test failures

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -x
-set +e
 
 cd "$GITHUB_WORKSPACE/array-api-tests"
 export ARRAY_API_TESTS_MODULE="$1"
-pytest array_api_tests/ --json-report
+pytest array_api_tests/ --json-report || true
